5d1d85e39fc8448e453874caf72c504c96343772
shigengtian <shigengtian@gmail.com>
2022-10-14 13:27:05 +0900

    u

---
+++ train.sh
@@ -0,0 +1,21 @@
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col cohesion --output exp010-fb3-deberta-v3-large
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col syntax --output exp010-fb3-deberta-v3-large
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col vocabulary --output exp010-fb3-deberta-v3-large
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col phraseology --output exp010-fb3-deberta-v3-large
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col grammar --output exp010-fb3-deberta-v3-large
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col conventions --output exp010-fb3-deberta-v3-large
+
+
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col cohesion --output exp010-fb3-deberta-v3-base
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col syntax --output exp010-fb3-deberta-v3-base
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col vocabulary --output exp010-fb3-deberta-v3-base
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col phraseology --output exp010-fb3-deberta-v3-base
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col grammar --output exp010-fb3-deberta-v3-base
+python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col conventions --output exp010-fb3-deberta-v3-base
+
+
+# 'cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions'
+
+# python exp009-fb3-deberta-v3-large-syntax.py
+# python exp009-fb3-deberta-v3-large-vocabulary.py
+# python exp08-fb3-deberta-v3-base-dropout.py
