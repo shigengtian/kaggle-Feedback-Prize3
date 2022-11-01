b6d1e81fd0de277a1bc9d103e34583d1ab03135d
shigengtian <shigengtian@gmail.com>
2022-11-01 09:54:53 +0900

    u

--- train.sh
+++ train.sh
@@ -1,26 +1,7 @@
-# python exp013-fb3-deberta-large.py
-# python exp014-fb3-deberta-v2-xlarge.py
-# python exp015-fb3-deberta-v3-base.py
-# python exp014-fb3-deberta-v2-xlarge.py
+python train_baseline.py --model microsoft/deberta-v3-base --lr 1e-5 --batch_size 8 --output_path exp015/
 
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col cohesion --output exp010-fb3-deberta-v3-large
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col syntax --output exp010-fb3-deberta-v3-large
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col vocabulary --output exp010-fb3-deberta-v3-large
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col phraseology --output exp010-fb3-deberta-v3-large
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col grammar --output exp010-fb3-deberta-v3-large
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-large --col conventions --output exp010-fb3-deberta-v3-large
-
-
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col cohesion --output exp010-fb3-deberta-v3-base
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col syntax --output exp010-fb3-deberta-v3-base
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col vocabulary --output exp010-fb3-deberta-v3-base
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col phraseology --output exp010-fb3-deberta-v3-base
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col grammar --output exp010-fb3-deberta-v3-base
-# python exp010-fb3-deberta-v3-large.py --model microsoft/deberta-v3-base --col conventions --output exp010-fb3-deberta-v3-base
-
-
-# 'cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions'
-
-# python exp009-fb3-deberta-v3-large-syntax.py
-# python exp009-fb3-deberta-v3-large-vocabulary.py
-# python exp08-fb3-deberta-v3-base-dropout.py
+# parser.add_argument("--model", type=str, required=True)
+# parser.add_argument("--lr", type=float, required=True)
+# parser.add_argument("--max_len", type=int, default=1024, required=False)
+# parser.add_argument("--batch_size", type=int, default=8, required=False)
+# parser.add_argument("--output_path", type=str, required=True)
