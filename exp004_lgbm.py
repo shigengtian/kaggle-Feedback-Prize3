8ed45ed468bc2c78aea7567248e353f5d0423ea4
Sifat Ahmed <sifat.ahmed@eii-net.co.jp>
2022-11-02 17:15:14 +0900

    u

---
+++ exp004_lgbm.py
@@ -0,0 +1,286 @@
+# ====================================================
+# Directory settings
+# ====================================================
+import os
+
+OUTPUT_DIR = './exp007-fb3-deberta-v3-large-adv/'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+# ====================================================
+# CFG
+# ====================================================
+class CFG:
+    wandb=False
+    competition='FB3'
+    _wandb_kernel='shigengtian'
+    debug=False
+    apex=True
+    print_freq=20
+    num_workers=4
+    model="microsoft/deberta-v3-large"
+    gradient_checkpointing=True
+    scheduler='cosine' # ['linear', 'cosine']
+    batch_scheduler=True
+    num_cycles=0.5
+    num_warmup_steps=0
+    epochs=4
+    encoder_lr=2e-5
+    decoder_lr=2e-5
+    min_lr=1e-6
+    eps=1e-6
+    betas=(0.9, 0.999)
+    batch_size=8
+    max_len=512
+    weight_decay=0.01
+    gradient_accumulation_steps=1
+    max_grad_norm=1000
+    target_cols=['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']
+    seed=42
+    n_fold=5
+    trn_fold=[0, 1, 2, 3, 4]
+    train=True
+    awp_eps= 1e-2
+    awp_lr= 1e-4,
+    
+if CFG.debug:
+    CFG.epochs = 2
+    CFG.trn_fold = [0]
+
+# ====================================================
+# Library
+# ====================================================
+import os
+import gc
+import re
+import ast
+import sys
+import copy
+import json
+import time
+import math
+import string
+import pickle
+import random
+import joblib
+import itertools
+import warnings
+warnings.filterwarnings("ignore")
+
+import scipy as sp
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+from tqdm.auto import tqdm
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold
+
+from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
+
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+from torch.optim import Adam, SGD, AdamW
+from torch.utils.data import DataLoader, Dataset
+
+import tokenizers
+import transformers
+print(f"tokenizers.__version__: {tokenizers.__version__}")
+print(f"transformers.__version__: {transformers.__version__}")
+from transformers import AutoTokenizer, AutoModel, AutoConfig
+from transformers import get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
+from transformers import DataCollatorWithPadding
+
+# from adversarial import AWP
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+def MCRMSE(y_trues, y_preds):
+    scores = []
+    idxes = y_trues.shape[1]
+    for i in range(idxes):
+        y_true = y_trues[:,i]
+        y_pred = y_preds[:,i]
+        score = mean_squared_error(y_true, y_pred, squared=False) # RMSE
+        scores.append(score)
+    mcrmse_score = np.mean(scores)
+    return mcrmse_score, scores
+
+
+def get_score(y_trues, y_preds):
+    mcrmse_score, scores = MCRMSE(y_trues, y_preds)
+    return mcrmse_score, scores
+
+
+# def get_logger(filename=OUTPUT_DIR+'train'):
+#     from logging import getLogger, INFO, StreamHandler, FileHandler, Formatter
+#     logger = getLogger(__name__)
+#     logger.setLevel(INFO)
+#     handler1 = StreamHandler()
+#     handler1.setFormatter(Formatter("%(message)s"))
+#     handler2 = FileHandler(filename=f"{filename}.log")
+#     handler2.setFormatter(Formatter("%(message)s"))
+#     logger.addHandler(handler1)
+#     logger.addHandler(handler2)
+#     return logger
+
+# LOGGER = get_logger()
+
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+seed_everything(seed=42)
+
+
+# ====================================================
+# Data Loading
+# ====================================================
+train = pd.read_csv('./feedback-prize-english-language-learning/train.csv')
+test = pd.read_csv('./feedback-prize-english-language-learning/test.csv')
+submission = pd.read_csv('./feedback-prize-english-language-learning/sample_submission.csv')
+
+# ====================================================
+# CV split
+# ====================================================
+Fold = MultilabelStratifiedKFold(n_splits=CFG.n_fold, shuffle=True, random_state=CFG.seed)
+for n, (train_index, val_index) in enumerate(Fold.split(train, train[CFG.target_cols])):
+    train.loc[val_index, 'fold'] = int(n)
+train['fold'] = train['fold'].astype(int)
+
+
+def prepare_input(cfg, text):
+    inputs = cfg.tokenizer.encode_plus(
+        text, 
+        return_tensors=None, 
+        add_special_tokens=True, 
+        #max_length=CFG.max_len,
+        #pad_to_max_length=True,
+        #truncation=True
+    )
+    for k, v in inputs.items():
+        inputs[k] = torch.tensor(v, dtype=torch.long)
+    return inputs
+
+
+class TestDataset(Dataset):
+    def __init__(self, cfg, df):
+        self.cfg = cfg
+        self.texts = df['full_text'].values
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        inputs = prepare_input(self.cfg, self.texts[item])
+        return inputs
+    
+
+class MeanPooling(nn.Module):
+    def __init__(self):
+        super(MeanPooling, self).__init__()
+        
+    def forward(self, last_hidden_state, attention_mask):
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+        sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded, 1)
+        sum_mask = input_mask_expanded.sum(1)
+        sum_mask = torch.clamp(sum_mask, min=1e-9)
+        mean_embeddings = sum_embeddings / sum_mask
+        return mean_embeddings
+    
+    
+class CustomModel(nn.Module):
+    def __init__(self, cfg, config_path=None, pretrained=False):
+        super().__init__()
+        self.cfg = cfg
+        if config_path is None:
+            self.config = AutoConfig.from_pretrained(cfg.model, output_hidden_states=True)
+            self.config.hidden_dropout = 0.
+            self.config.hidden_dropout_prob = 0.
+            self.config.attention_dropout = 0.
+            self.config.attention_probs_dropout_prob = 0.
+        else:
+            self.config = torch.load(config_path)
+        if pretrained:
+            self.model = AutoModel.from_pretrained(cfg.model, config=self.config)
+        else:
+            self.model = AutoModel(self.config)
+        if self.cfg.gradient_checkpointing:
+            self.model.gradient_checkpointing_enable()
+        self.pool = MeanPooling()
+        self.fc = nn.Linear(self.config.hidden_size, 6)
+        self._init_weights(self.fc)
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        
+    def feature(self, inputs):
+        outputs = self.model(**inputs)
+        last_hidden_states = outputs[0]
+        feature = self.pool(last_hidden_states, inputs['attention_mask'])
+        return feature
+
+    def forward(self, inputs):
+        feature = self.feature(inputs)
+        output = self.fc(feature)
+        return output
+
+# ====================================================
+# inference
+# ====================================================
+def inference_fn(test_loader, model, device):
+    preds = []
+    model.eval()
+    model.to(device)
+    tk0 = tqdm(test_loader, total=len(test_loader))
+    for inputs in tk0:
+        for k, v in inputs.items():
+            inputs[k] = v.to(device)
+        with torch.no_grad():
+            y_preds = model(inputs)
+        preds.append(y_preds.to('cpu').numpy())
+    predictions = np.concatenate(preds)
+    return predictions
+
+def predict():
+    test_dataset = TestDataset(CFG, test)
+    test_loader = DataLoader(test_dataset,
+                             batch_size=CFG.batch_size,
+                             shuffle=False,
+                             collate_fn=DataCollatorWithPadding(tokenizer=CFG.tokenizer, padding='longest'),
+                             num_workers=CFG.num_workers, pin_memory=True, drop_last=False)
+    predictions = []
+    for fold in CFG.trn_fold:
+        model = CustomModel(CFG, config_path=CFG.config_path, pretrained=False)
+        state = torch.load(CFG.path+f"{CFG.model.replace('/', '-')}_fold{fold}_best.pth",
+                           map_location=torch.device('cpu'))
+        model.load_state_dict(state['model'])
+        prediction = inference_fn(test_loader, model, device)
+        predictions.append(prediction)
+        del model, state, prediction; gc.collect()
+        torch.cuda.empty_cache()
+    predictions = np.mean(predictions, axis=0)
+    return predictions
+
+predictions = predict()
